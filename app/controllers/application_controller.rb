983822035c3087aeadddeb88905fc7ef7abfc2fa
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-10-23 09:36:06 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, '../views/') }
 
   configure do
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, 'secret'
   end
 
   get '/' do
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user = User.new(name: params['name'], email: params['email'], password: params['password'])
     @user.save
     session[:user_id] = @user.id
 
