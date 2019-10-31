3b3ccbd689bdf785331ddfb182ca412a5c982c14
Dominique Skaf <doskaf@yahoo.com>
2019-10-30 21:59:36 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,14 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
+  post '/registrations' do
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:user_id] = @user.id
+
+    redirect '/users/home'
+  end
+
   get '/registrations/signup' do
 
     erb :'/registrations/signup'
